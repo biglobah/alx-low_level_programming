e36ff374fbe1ef0dabf1ff249b7b3c374cfa46f7
biglobah11 <biglobah11@gmail.com>
2022-10-13 18:54:03 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/*main function to return a void value*/
+int main(void)
+{
+/*Using the put command to print*/
+	puts("\"Programming is like building a multilingual puzzle"):
+	return (0):
+}
