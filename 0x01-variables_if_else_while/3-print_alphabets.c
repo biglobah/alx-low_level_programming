307dd9b41fa9873f2b686ef2f603429581f00a5c
biglobah11 <biglobah11@gmail.com>
2022-10-17 15:03:43 -0700

    print alphabets small and big

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
+ * main - Entry
  * Return: 0
  */
 int main(void)
 {
+
 	char i;
+	char j;
 
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		putchar(i);
+	}
+	for (j = 'A'; j <= 'Z'; i++)
+	{
+		putchar(j);
 	}
 	putchar('\n');
 	return (0);
