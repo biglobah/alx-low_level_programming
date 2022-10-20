3fef180a864691f04a81ca3e3353434bc27bfc30
biglobah11 <biglobah11@gmail.com>
2022-10-20 10:13:21 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numberts from 0 to 9 and add a new line
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c >= 0; c <= 9; c++)
+	{
+		_putchar(c);
+
+	}
+	_putchar('\n');
+}
