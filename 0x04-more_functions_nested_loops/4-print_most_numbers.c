e0282cb0b0277ecace0753f80ce47232f121a5b2
biglobah11 <biglobah11@gmail.com>
2022-10-20 13:37:17 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,18 +8,13 @@
 void print_most_numbers(void)
 {
 	int a;
-	{
+
 		for (a = 0; a < 10; a++)
 		{
-			if (a != 2)
-			{
-				if (a != 4)
-				{
-					_putchar(a);
-				}
-			}
-			_putchar('\n');
+			if (a != 2 && a != 4)
+				_putchar(a);
 		}
-	}
+		_putchar('\n');
 }
+
 
